rasky/g64drive

commit aa835c1fdf9ba966d8115f88fa633e8f3a76872d

Small fix to allow uploading a buffer whose size is not a multiple of 8 bytes.

drive64/drive64.go
@@ -216,7 +216,7 @@ func (d *Device) CmdUpload(ctx context.Context, r io.Reader, n int64, bank Bank,
 	chunkSize := idealChunkSize(n)
 	d.usb.SetWriteChunkSize(chunkSize + 12)
 
-	for n != 0 && ctx.Err() == nil {
+	for n >= 0 && ctx.Err() == nil {
 		sz := chunkSize
 		if n > 0 && int64(sz) > n {
 			sz = int(n)
@@ -231,6 +231,12 @@ func (d *Device) CmdUpload(ctx context.Context, r io.Reader, n int64, bank Bank,
 		}
 		if err = bs.ByteSwap(buf); err != nil {
 			return err
+		}
+
+		// Transfer must be multiple of 8. Pad with zeros
+		for len(buf)%8 != 0 {
+			buf = append(buf, 0)
+			read += 1
 		}
 
 		cmdargs[1] = uint32(bank)<<24 | uint32(read)

commit c0fd602d6692b64d35164fae89f8efd64bbebceb

Fix regression in upload command introduced in aa835c1f

drive64/drive64.go
@@ -246,7 +246,7 @@ func (d *Device) CmdUpload(ctx context.Context, r io.Reader, n int64, bank Bank,
 	chunkSize := idealChunkSize(n)
 	d.usb.SetWriteChunkSize(chunkSize + 12)
 
-	for n >= 0 && ctx.Err() == nil {
+	for n > 0 && ctx.Err() == nil {
 		sz := chunkSize
 		if n > 0 && int64(sz) > n {
 			sz = int(n)